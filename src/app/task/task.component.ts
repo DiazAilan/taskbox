2d20056ec16271db7c2a217527a370c7b6ff2ba7
DiazAilan <diaz.ailan.gonzalo@gmail.com>
2025-02-16 15:51:09 +0100

    feat: add Task component

---
+++ src/app/task/task.component.ts
@@ -0,0 +1,22 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-task',
+  standalone: false,
+  templateUrl: './task.component.html',
+  styleUrl: './task.component.css'
+})
+export default class TaskComponent {
+  /**
+   * The shape of the task object
+  */
+  @Input() task: any;
+
+  // tslint:disable-next-line: no-output-on-prefix
+  @Output()
+  onPinTask = new EventEmitter<Event>();
+
+  // tslint:disable-next-line: no-output-on-prefix
+  @Output()
+  onArchiveTask = new EventEmitter<Event>();
+}

f82115a0dd1bc310158b6f63e0c3d279633fa8fe
DiazAilan <diaz.ailan.gonzalo@gmail.com>
2025-02-18 10:51:20 +0100

    feature: add filteredTasks behavior and emptyTask behavior to TaskList component

--- src/app/task-list/task-list.component.ts
+++ src/app/task-list/task-list.component.ts
@@ -8,8 +8,23 @@ import { Task } from '../models/task.model';
   styleUrl: './task-list.component.css'
 })
 export class TaskListComponent {
-  @Input() tasks: Task[] = [];
+  
+  tasksInOrder: Task[] = [];
+
   @Input() loading = false;
   @Output() onPinTask = new EventEmitter<Event>();
   @Output() onArchiveTask = new EventEmitter<Event>();
+
+  @Input() set tasks(arr: Task[]) {
+    const initialTasks = [
+      ...arr.filter(t => t.state === 'TASK_PINNED'),
+      ...arr.filter(t => t.state !== 'TASK_PINNED'),
+    ];
+    const filteredTasks = initialTasks.filter(
+      t => t.state === 'TASK_INBOX' || t.state === 'TASK_PINNED'
+    );
+    this.tasksInOrder = filteredTasks.filter(
+      t => t.state === 'TASK_INBOX' || t.state === 'TASK_PINNED'
+    );
+  }
 }

f2a79ca26135021b7d9ddfd505eb901189ea74f3
DiazAilan <diaz.ailan.gonzalo@gmail.com>
2025-02-22 15:15:34 +0100

    fix: add missing ellipsis for long Task titles

--- src/app/components/inbox-screen/inbox-screen.component.ts
+++ src/app/components/inbox-screen/inbox-screen.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-inbox-screen',
@@ -7,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrl: './inbox-screen.component.scss'
 })
 export default class InboxScreenComponent {
-
+  @Input() error$: Observable<any> = of(null);
 }
